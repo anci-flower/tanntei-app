3d58718743d53c67308036e83524f139c0e2b22c
anci-flower <2289342907@qq.com>
2019-06-03 20:21:06 +0800

    Updates

---
+++ precache-manifest.ce0731d3d0b6a9fd72dd32da70718ef4.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "82aa38d5bff601d0270d941553d18cc9",
+    "url": "/tanntei-app/static/media/pic3.82aa38d5.jpg"
+  },
+  {
+    "revision": "e2234a545d291a68e7df",
+    "url": "/tanntei-app/static/css/main.1cf64130.chunk.css"
+  },
+  {
+    "revision": "0d7e5593f951eceaa5bd",
+    "url": "/tanntei-app/static/js/runtime~main.c51fa768.js"
+  },
+  {
+    "revision": "fdf387f0dccce89a1ad18dba3b89d8bc",
+    "url": "/tanntei-app/static/media/header_bg.fdf387f0.jpg"
+  },
+  {
+    "revision": "befa925673f40efe7899",
+    "url": "/tanntei-app/static/js/2.75ad97e7.chunk.js"
+  },
+  {
+    "revision": "03bb9098728005510d670bcf94223119",
+    "url": "/tanntei-app/static/media/pic1.03bb9098.jpg"
+  },
+  {
+    "revision": "4b2430ac7b02e567e0ad1ef6e9fb3259",
+    "url": "/tanntei-app/static/media/pic2.4b2430ac.jpg"
+  },
+  {
+    "revision": "e2234a545d291a68e7df",
+    "url": "/tanntei-app/static/js/main.4c209fb5.chunk.js"
+  },
+  {
+    "revision": "ae76859162efd2fa71e2a4b8eb84753b",
+    "url": "/tanntei-app/static/media/water-1.ae768591.svg"
+  },
+  {
+    "revision": "4de7ae754a1503c08efda5301c263d95",
+    "url": "/tanntei-app/static/media/water-2.4de7ae75.svg"
+  },
+  {
+    "revision": "8c73432ae6504820cafa2162ceb7700f",
+    "url": "/tanntei-app/static/media/iconfont.8c73432a.eot"
+  },
+  {
+    "revision": "ee87f6a5fa989edf4a1ff5483a573143",
+    "url": "/tanntei-app/static/media/iconfont.ee87f6a5.ttf"
+  },
+  {
+    "revision": "3258f9a9c4360017a5e191748becbb22",
+    "url": "/tanntei-app/static/media/iconfont.3258f9a9.svg"
+  },
+  {
+    "revision": "befa925673f40efe7899",
+    "url": "/tanntei-app/static/css/2.90ebf25c.chunk.css"
+  },
+  {
+    "revision": "271e1ee7d11839ef85a0bf333eeba853",
+    "url": "/tanntei-app/index.html"
+  }
+];
